5ae4b2a4cb5e3e6eeab1c33bd145724d4cfe428a
TheHarmanCodes <harmansingh444444@gmail.com>
2026-02-12 19:46:36 +0530

    Formatting the function decelatations to maintain the clear white space

--- C/stack/stack_using_array.c
+++ C/stack/stack_using_array.c
@@ -1,8 +1,53 @@
-// Stack implementation using Array ,call by address , call by value for top
+// Problem Statement: C program to implement Stack (ADT) using Array, Mixed: Call by Address, call by value of top
+
 #include <stdio.h>
 #include <stdlib.h>
 
 #define max 5
+
+int push(int[], int);
+int pop(int[], int);
+void peek(int[], int);
+void display(int[], int);
+
+int main()
+{
+    int stack[max], top = -1, choice;
+
+    while (1)
+    {
+        system("cls");
+
+        printf("\n Stack\n------\n1: Push\n2: Pop\n3: Peek\n4: Display\n5: Exit\nEnter Choice: ");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+        case 1:
+            top = push(stack, top);
+            break;
+        case 2:
+            top = pop(stack, top);
+            break;
+        case 3:
+            peek(stack, top);
+            break;
+        case 4:
+            display(stack, top);
+            break;
+        case 5:
+            printf("\nExiting...");
+            return 0;
+        default:
+            printf("\nInvalid Choice\tTry Again\n");
+            break;
+        }
+        printf("\nPress Enter to continue...");
+        getchar();
+        getchar();
+    }
+    return 0;
+}
 
 int push(int stack[], int top)
 {
@@ -46,6 +91,7 @@ void peek(int stack[], int top)
         printf("\nTop element is: %d\n", stack[top]);
     }
 }
+
 void display(int stack[], int top)
 {
     if (top == -1)
@@ -61,43 +107,4 @@ void display(int stack[], int top)
             top--;
         }
     }
-}
-
-int main()
-{
-    int stack[max], top = -1, choice;
-
-    while (1)
-    {
-        system("cls");
-
-        printf("\n Stack\n------\n1: Push\n2: Pop\n3: Peek\n4: Display\n5: Exit\nEnter Choice: ");
-        scanf("%d", &choice);
-
-        switch (choice)
-        {
-        case 1:
-            top = push(stack, top);
-            break;
-        case 2:
-            top = pop(stack, top);
-            break;
-        case 3:
-            peek(stack, top);
-            break;
-        case 4:
-            display(stack, top);
-            break;
-        case 5:
-            printf("\nExiting...");
-            return 0;
-        default:
-            printf("\nInvalid Choice\tTry Again\n");
-            break;
-        }
-        printf("\nPress Enter to continue...");
-        getchar();
-        getchar();
-    }
-    return 0;
 }
